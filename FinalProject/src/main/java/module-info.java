8ea4f025b14b1c87081aefa52aeb6c979fe288cd
Joker <Joker@DESKTOP-564BPNQ>
2023-05-10 12:32:16 +1000

    Name Change

---
+++ FinalProject/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module jjdcontracting.finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens jjdcontracting.finalproject to javafx.fxml;
+    exports jjdcontracting.finalproject;
+}
