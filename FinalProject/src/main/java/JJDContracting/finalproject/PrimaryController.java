9dc2a4f4052934fd27331a1e184880522c1033a8
Joker <Joker@DESKTOP-564BPNQ>
2023-05-10 12:17:59 +1000

    Name Change

---
+++ FinalProject/src/main/java/JJDContracting/finalproject/PrimaryController.java
@@ -0,0 +1,12 @@
+package JJDContracting.finalproject;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
