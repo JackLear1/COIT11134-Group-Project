461c76b131a0be6565605695f2d99bada752e0f7
JENNA <JENNA@Jenna>
2023-06-07 16:05:41 +1000

    search single user code

--- FinalProject/src/main/java/jjdcontracting/finalproject/ViewSingleUsersController.java
+++ FinalProject/src/main/java/jjdcontracting/finalproject/ViewSingleUsersController.java
@@ -13,39 +13,62 @@ public class ViewSingleUsersController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }   
-    
+
+    }
+
     //will allow user to search through user records
     @FXML
     private void Search() throws IOException {
-        
+
+        int staffID = Integer.parseInt(staffIdTextField.getText());
+
+        for (int i = 0; i < App.user.size(); i++) {
+            if (App.user.get(i).getStaffID() == staffID) {
+
+                int foundID = i;
+                recallInfo(foundID);
+                break;
+            } else {
+                staffIdTextField.setText("StaffID number not found.");
+
+            }
+        }
     }
-    
-    //will save edits that user has made to record
+
+    private void recallInfo(int id) {
+        staffNameTextField.setText(App.user.get(id).getStaffName());
+        staffIdTextField.setText(App.user.get(id).getStaffID);
+        staffExtTextField.setText(App.user.get(id).getStaffExt());
+        licenseNumberTextField.setText(App.user.get(id).getLicenseNumber());
+        licenseExpiryTextField.setText(App.user.get(id).getLicenseExpiry());
+        manualLicenseTextField.setText(App.user.get(id).getManualLicense());
+        busLicenseTextField.setText(App.user.get(id).getBusLicense());
+    }
+
+//will save edits that user has made to record
     @FXML
     private void Save() throws IOException {
-        
+
     }
-    
+
     //will allow user to edit currently viewed record
     @FXML
     private void Edit() throws IOException {
-        
+
     }
-    
+
     //will delete currently viewed record
     @FXML
     private void Delete() throws IOException {
-        
+
     }
-    
+
     //will take user back to MainMenu
     @FXML
     private void Back() throws IOException {
         App.setRoot("MainMenu");
     }
-    
+
     //will ask for user confirmation before closing program
     @FXML
     private void exitClick() {
@@ -56,5 +79,5 @@ public class ViewSingleUsersController implements Initializable {
             }
         });
     }
-    
+
 }
